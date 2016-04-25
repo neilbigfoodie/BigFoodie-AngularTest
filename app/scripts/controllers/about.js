b3212d89ad69d414ac17531aee14ad31381de636
neilbigfoodie <neil.afflick@bigfoodie.co.uk>
2016-04-25 10:14:28 +0100

    intial-addition-of-existing-code.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bigFoodieAngularTestApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the bigFoodieAngularTestApp
+ */
+angular.module('bigFoodieAngularTestApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bigFoodieAngularTestApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bigFoodieAngularTestApp
+ */
+angular.module('bigFoodieAngularTestApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
